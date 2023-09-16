01a17e323d92695ddb3d9d8ba5959f954b84faf6
aadrsh26288 <78693934+aadrsh26288@users.noreply.github.com>
2023-09-16 16:27:05 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -7,11 +7,13 @@ const cors = require("cors");
 
 app.use(cors());
 
-app.use(
-	cors({
-		origin: "https://recipe-genius.vercel.app/",
-	}),
-);
+const corsOptions = {
+	origin: "https://recipe-genius.vercel.app/",
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
 // Allow specific origin(s)
 
 app.use(express.json());
